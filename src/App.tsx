4f05217c6865911b5fdba0268544208d8c81f23c
Dmitry DM <ultrwave@gmail.com>
2021-03-10 21:10:31 +0300

    merge bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import style from './App.module.css';
 import {HashRouter, Route} from 'react-router-dom';
-import Login from './content/Login';
 import Password from './content/Password';
 import Profile from './content/Profile';
 import Recover from './content/Recover';
 import Registration from './content/Registration';
 import {Navbar} from './common/Navbar';
-import SuperInputsDemo from "./content/SuperInputsDemo";
+import SuperInputsDemo from './content/SuperInputsDemo';
+import LoginContainer from './content/Login/LoginContainer';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 function App() {
     return (
         <HashRouter>
-            <div className="App">
-                <Navbar/>
-                <div className={style.content}>
-                    <Route path='/login' render={() => <Login/>}/>
-                    <Route path='/password' render={() => <Password/>}/>
-                    <Route path='/profile' render={() => <Profile/>}/>
-                    <Route path='/recover' render={() => <Recover/>}/>
-                    <Route path='/registration' render={() => <Registration/>}/>
-                    <Route path='/demo' render={() => <SuperInputsDemo/>}/>
+            <Provider store={store}>
+                <div className="App">
+                    <Navbar/>
+                    <div className={style.content}>
+                        <Route path='/login' render={() => <LoginContainer/>}/>
+                        <Route path='/password' render={() => <Password/>}/>
+                        <Route path='/profile' render={() => <Profile/>}/>
+                        <Route path='/recover' render={() => <Recover/>}/>
+                        <Route path='/registration' render={() => <Registration/>}/>
+                        <Route path='/demo' render={() => <SuperInputsDemo/>}/>
+                    </div>
                 </div>
-            </div>
+            </Provider>
         </HashRouter>
     );
 }
