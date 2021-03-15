4770de13c9603aabb82cb3c0c8878c1e2d3d5653
Dmitry DM <ultrwave@gmail.com>
2021-03-15 20:06:05 +0300

    cardsAPI

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -4,9 +4,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'http://localhost:7542/2.0/',
     // baseURL: 'https://neko-back.herokuapp.com/2.0',
-    headers: {
-
-    }
+    headers: {}
 })
 
 export const authAPI = {
@@ -19,7 +17,7 @@ export const authAPI = {
             .then(response => response.data)
     },
     logout() {
-      return instance.delete('/auth/me')
+        return instance.delete('/auth/me')
     },
     forgot(email: string, from: string, message: string) {
         return instance.post('/auth/forgot', {email, from, message})
@@ -27,8 +25,8 @@ export const authAPI = {
     setNewPassword(password: string, resetPasswordToken: string) {
         return instance.post('/auth/set-new-password', {password, resetPasswordToken})
     },
-    registerUser(email:string,password:string) {
-        return  instance.post('/auth/register', {email, password})
+    registerUser(email: string, password: string) {
+        return instance.post('/auth/register', {email, password})
     },
 }
 
@@ -37,13 +35,28 @@ export const packsAPI = {
         return instance.get('/cards/pack') //todo - params?
     },
     createPack(data: CreatePackType) {
-      return instance.post('/cards/pack', data)
+        return instance.post('/cards/pack', data)
     },
     deletePack(id: string) {
         return instance.delete(`/cards/pack?id=${id}`)
     },
     updatePack(cardsPack: PackType) {
         return instance.put('/cards/pack', cardsPack)
+    }
+}
+
+export const cardsAPI = {
+    getCards() {
+        return instance.get('/cards/card') //todo - params?
+    },
+    createCard(data: CreatePackType) {
+        return instance.post('/cards/card', data)
+    },
+    deleteCard(id: string) {
+        return instance.delete(`/cards/card?id=${id}`)
+    },
+    updateCard(cardsPack: PackType) {
+        return instance.put('/cards/card', cardsPack)
     }
 }
 
@@ -62,4 +75,43 @@ export type CreatePackType = { // todo - оранж поля?
     deckCover?: string // не обязателен
     private?: boolean // если не отправить будет такой
     type?: string // если не отправить будет таким
+}
+
+export type CardType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    rating: number
+    shots: number
+    type: string
+    user_id: string
+    created: string
+    updated: string
+    __v: number
+    _id: string
+}
+
+export type CreateCardType = {
+    answer: string // если не отправить будет таким
+    question: string // если не отправить будет таким
+    cardsPack_id: string
+    grade: number // 0..5, не обязателен
+    rating: number // не обязателен
+    shots: number // не обязателен
+    type: string // если не отправить будет таким
+    answerImg: string // не обязателен
+    questionImg: string // не обязателен
+    questionVideo: string // не обязателен
+    answerVideo: string // не обязателен
+}
+
+export type CardsResponseType = {
+    cards: CardType[]
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
 }
