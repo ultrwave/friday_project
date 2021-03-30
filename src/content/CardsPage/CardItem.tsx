38b904c756418f1c26a363e1796f0f71a5cf8108
Yury Vinogradov <yuvinogradov@gmail.com>
2021-03-30 19:51:27 +0300

    fixed Delete button in CardItem.tsx

--- src/content/CardsPage/CardItem.tsx
+++ src/content/CardsPage/CardItem.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
 import style from '../styles/PacksPage.module.css'
 import {CardType} from '../../api/AuthAPI';
 import {useSelector} from 'react-redux';
 import {RootStateType} from '../../redux/store';
+import ModalQuestionContainer from "../../common/modals/question/ModalQuestionContainer";
 
 type CardItemPropsType = {
     deleteCallback(): void
@@ -21,21 +21,37 @@ function CardItem(props: CardType & CardItemPropsType) {
     const created = new Date(props.created)
         .toLocaleDateString("en-UE", {hour12: false, hour: 'numeric', minute: 'numeric'});
 
+    const deleteHandler = (answer: boolean) => {
+        if (answer) {
+            props.deleteCallback()
+        }
+    }
+
     return (
         <li>
-            <div className={`${style.packItem}${itemIsMine? '' : (' ' + style.itemIsNotMine)}`}>
+            <div className={`${style.packItem}${itemIsMine ? '' : (' ' + style.itemIsNotMine)}`}>
                 <div style={{width: '15%'}}>{props.question}</div>
                 <div style={{width: '10%'}}>{props.answer}</div>
                 <div style={{width: '15%'}}>
                     {Math.round((props.grade + Number.EPSILON) * 100) / 100}</div>
                 <div style={{width: '10%', fontSize: '12px'}}>{updated}</div>
                 <div style={{width: '10%', fontSize: '12px'}}>{created}</div>
-                <div style={{width: '15%'}}>
-                    <button disabled={!itemIsMine}
-                            onClick={props.deleteCallback}>Delete</button>
+                <div style={{width: '15%', display: 'flex', flexDirection: 'row'}}>
+                    {/*<button disabled={!itemIsMine}*/}
+                    {/*        onClick={props.deleteCallback}>Delete</button>*/}
+                    <ModalQuestionContainer buttonTitle={'Delete'}
+                                            // modalText={'Delete card?'}
+                                            isMine={itemIsMine}
+                                            answerCallback={deleteHandler}>
+                        <>
+                            <div style={{fontSize: '2em'}}>{props.question}</div>
+                            <div>Delete card?</div>
+                        </>
+                    </ModalQuestionContainer>
                     <button style={{marginLeft: '5px'}}
                             disabled={!itemIsMine}
-                            onClick={props.updateCallback}>Update</button>
+                            onClick={props.updateCallback}>Update
+                    </button>
                 </div>
                 <div style={{width: '25%'}}/>
             </div>
